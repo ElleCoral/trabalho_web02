58e2b122641ee6a3b0d966bc51cf15c081374844
Trabalho_web02 <gabrielle.coral@hotmail.com>
2024-11-23 01:21:28 -0300

    Primeiro commit

---
+++ src/app/cadastro/page.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+
+// pages/page.js
+const Page = () => {
+    return <h1>Página de Exemplo</h1>;
+  };
+  
+  export default Page;
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+// pages/page.js
+const Page = () => {
+  return <h1>Página de Exemplo</h1>;
+};
+export default Page;
