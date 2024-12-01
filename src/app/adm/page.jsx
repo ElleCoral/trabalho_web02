b58c2302c74b39e0883f4d771897b7eceb99b72e
Trabalho_web02 <gabrielle.coral@hotmail.com>
2024-12-01 20:38:30 -0300

    Modificações e exclusão de arquivos na página admin

--- src/app/adm/page.jsx
+++ src/app/adm/page.jsx
@@ -1,8 +1,47 @@
-import './style.css';
+"use client";
 
-const Page = () => {
-    return <h1>Seja Bem-Vindo, Admin</h1>;
-  };
-  
-  export default Page;
+import Link from "next/link";
+import { LogIn } from "lucide-react";
+import "./style.css";
+
+const Sidebar = () => {
+  return (
+    <div className="container">
+      <div className="titulo">
+        <h1>Seja Bem-vindo, Administrador!</h1>
+      </div>
+
+      <div className="sidebar">
+        <h2>Menu</h2>
+        <ul>
+          <li>
+            <a href="/cadastro">Cadastro de Professores</a>
+          </li>
+          <li>
+            <a href="/cadastro">Cadastro de Alunos</a>
+          </li>
+          <li>
+            <a href="/cadastro">Cadastro de Eventos</a>
+          </li>
+          <li>
+            <a href="/cadastro">Cadastro de Agendamentos</a>
+          </li>
+          <li>
+            <a href="/cadastro">Cadastro de Profissionais</a>
+          </li>
+          <li>
+            <a href="/cadastro">Cadastro de Usuários</a>
+          </li>
+        </ul>
+        <div className="login-icon">
+          <Link href="/login">
+            <LogIn size={30} color="#efefef" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
 
--- src/app/page.jsx
+++
@@ -1,4 +0,0 @@
-const Page = () => {
-  return <h1>Página de Exemplo</h1>;
-};
-export default Page;
